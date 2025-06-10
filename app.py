729c8ea3c2ff5853bc81b410a914167cec0e485c
Joao <joao.jr500@gmail.com>
2025-06-09 22:01:07 -0300

    atualizando funcoes

--- app.py
+++ app.py
@@ -60,5 +60,5 @@ def lista_residentes():
     return render_template("lista_residentes.html")
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True)
