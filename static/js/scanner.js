bbe4841014e40422c7228007d7f21f95a3f2c1e8
Joao <joao.jr500@gmail.com>
2025-06-11 09:34:39 -0300

    adicionando funcao de abrir camera e ler QRCode

--- static/js/scanner.js
+++ static/js/scanner.js
@@ -1,40 +1,45 @@
-  let scanner = null;
+let scanner = null;
 
-  function startScanner() {
-    document.getElementById("scanner-modal").style.display = "flex";
+function startScanner() {
+  document.getElementById("scanner-modal").style.display = "flex";
 
-    scanner = new Html5Qrcode("reader");
+  scanner = new Html5Qrcode("reader");
 
-    const config = {
-      fps: 10,
-      qrbox: { width: 250, height: 250 }
-    };
+  const config = {
+    fps: 10,
+    qrbox: { width: 250, height: 250 }
+  };
 
-    scanner.start(
-      { facingMode: "environment" },
-      config,
-      qrCodeMessage => {
-        alert("QR Code detectado: " + qrCodeMessage);
-        stopScanner();
-      },
-      errorMessage => {
-        console.log("Erro de leitura:", errorMessage);
-      }
-    ).catch(err => {
-      console.error("Erro ao iniciar scanner:", err);
-    });
-  }
-
-  function stopScanner() {
-    if (scanner) {
-      scanner.stop().then(() => {
-        scanner.clear();
-        scanner = null;
-        document.getElementById("scanner-modal").style.display = "none";
-      }).catch(err => {
-        console.error("Erro ao parar o scanner:", err);
+  scanner.start(
+    { facingMode: "environment" },
+    config,
+    qrCodeMessage => {
+      Swal.fire({
+        icon: 'success',
+        title: 'Presença registrada com sucesso!',
+        showConfirmButton: false,
+        timer: 2000
       });
-    } else {
-      document.getElementById("scanner-modal").style.display = "none";
+      stopScanner();
+    },
+    errorMessage => {
+      console.log("Erro de leitura:", errorMessage);
     }
+  ).catch(err => {
+    console.error("Erro ao iniciar scanner:", err);
+  });
+}
+
+function stopScanner() {
+  if (scanner) {
+    scanner.stop().then(() => {
+      scanner.clear();
+      scanner = null;
+      document.getElementById("scanner-modal").style.display = "none";
+    }).catch(err => {
+      console.error("Erro ao parar o scanner:", err);
+    });
+  } else {
+    document.getElementById("scanner-modal").style.display = "none";
   }
+}
